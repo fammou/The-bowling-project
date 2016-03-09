f8a820909351c6d1a053d1b31712e96d206bc49e
YANLINQU <quyanlingg@hotmail.com>
2016-03-09 14:22:33 +0100

    rajouter les scores dans la tableau

--- src/main/java/Score.java
+++ src/main/java/Score.java
@@ -5,4 +5,15 @@
 
 public class Score {
     private int[] itsThrows = new int[31];//tableau des Scores
+    private int ball;//le numero du lance
+    private int itsCurrentThrow =  0;//le numero du lance en current
+
+    /**
+     * rajouter les scores dans la tableau
+     * itsThrows[le numero du lance]=score
+     * @param pins:score
+     */
+    public void addThrow(int pins){
+        itsThrows[itsCurrentThrow++]=pins;
+    }
 }
