be33d8acf0f30c45af2876a9ff9b451e9fc57096
fammou <sowaoumar25@gmail.com>
2016-03-09 15:26:37 +0100

    #4 Rajoute des methodes à impémenté dans la classe joueur

--- src/main/java/Joueur.java
+++ src/main/java/Joueur.java
@@ -3,6 +3,40 @@
  */
 public class Joueur {
 
+    private int itsCurrentFrame = 0;//le numero de frame
+    private boolean firstThrowInFrame = true;//le jou premier fois sur une frame
+    private Scorer itsScorer = new Scorer();//le compteur score
+
     public Joueur(){
     }
+
+
+    public int score(){
+        return 0;
+    }
+
+    public void add(int pins){
+    }
+
+
+    private void adjustCurrentFrame(int pins){
+    }
+
+    private boolean lastBallInFrame(int pins){
+        return true;
+    }
+
+
+    private boolean stricke(int pins){
+        return true;
+    }
+
+
+    private void advanceFrame(){
+    }
+
+    public int scoreForFrame(int theFrame){
+        return 0;
+    }
+
 }
