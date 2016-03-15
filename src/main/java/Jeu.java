0193b3b64d3e119e1e5f6802eb18fd86048d62ee
fammou <sowaoumar25@gmail.com>
2016-03-15 10:48:33 +0100

    Fin de la version 1 du jeu

--- src/main/java/Jeu.java
+++ src/main/java/Jeu.java
@@ -8,21 +8,19 @@ public class Jeu {
         int scores[]={10,6,6,  6,4,7,  5,5,5,  4,5,   10,10,10,   10,10,10,   9,1,10,   6,4,10,
                 10,10,10,   10,10,10};
 
-        int scores2[]={10,6,6,  7,3,7,  5,5,5,  4,4,   6,4,1,  10,1,7,   4,5,   6,4,10,
-                10,10,10,   6,4,7};
+        int scores2[]={6,4,7,  7,3,7,  5,5,5,  4,4,   10,10,10,  10,1,7,   4,5,   6,4,10,
+                10,10,10,   10,10,10,};
 
         Joueur g = new Joueur();
         Joueur g2 = new Joueur();
 
         for(int i=0;i<scores.length;i++){
             g.add(scores[i]);
-            System.out.println(g.score());
         }
 
 
         for(int i=0;i<scores2.length;i++){
             g2.add(scores2[i]);
-            System.out.println(g2.score());
         }
 
         for (int j = 1; j < 11; j++) {
